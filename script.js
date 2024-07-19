ad5c82da8e6dfe4322e3c50cfe329ec027534a01
Santoshreddy2063 <153976859+Santoshreddy2063@users.noreply.github.com>
2024-07-20 00:02:42 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// script.js
-window.addEventListener('scroll', function() {
-    const navbar = document.querySelector('.navbar');
-    if (window.scrollY > 50) {
-        navbar.style.background = 'linear-gradient(to right, #0044cc, #0099cc)';
-    } else {
-        navbar.style.background = 'linear-gradient(to right, #0066ff, #00ccff)';
-    }
-});
